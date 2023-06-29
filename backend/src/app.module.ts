1e52553873b07c58761e577e6850b4409303c419
ssd-fl <worldsr.up@gmail.com>
2023-06-28 21:50:29 -0400

    Update mongo db url with mongodb atlas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { KpisModule } from './kpi/kpis.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/dg'), KpisModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://worldsr:croco@dg.qb9riq9.mongodb.net',
+    ),
+    KpisModule,
+  ],
 })
 export class AppModule {}

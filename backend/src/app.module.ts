de14f6b689fcc16e06441e65607c8254603951a0
Software engineer <57792767+w-sr@users.noreply.github.com>
2023-06-29 03:10:37 -0400

    Add env vars (#6)
    Add env vars

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { KpisModule } from './kpi/kpis.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://worldsr:croco@dg.qb9riq9.mongodb.net',
-    ),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URL'),
+      }),
+    }),
     KpisModule,
   ],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import * as cors from 'cors';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +16,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  // app.use(cors());
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  });
+
   await app.listen(3001);
 }
+
 bootstrap();
